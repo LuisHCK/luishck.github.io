3d32b0b19409c5b82fbe05ed29a374749fafc209
LuisHCK <luisjcenteno@live.com>
2021-02-16 04:09:35 +0000

    Deploying to gh-pages from @ LuisHCK/luishck.github.io@e172629038e53558cd595507415f071fd7e9d7a1 🚀

--- README.md
+++
@@ -1,3 +0,0 @@
-## Awesome Web Developer Portfolio (AWDP)
-
-![screenshot-v1](https://i.ibb.co/xfLRcY4/e0a17486-7745-4561-5275-307aee3c8471.png)
--- utils/index.js
+++
@@ -1,4 +0,0 @@
-import markdownFetch from './markdownFetch'
-import getProjectContent from './getProjectContent'
-
-export { markdownFetch, getProjectContent }
